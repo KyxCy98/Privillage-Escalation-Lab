24c7bf73bb8bbeaff852f85549f47cbc8602318b
KyxCy98 <zaxmr025@gmail.com>
2026-03-17 22:03:02 +0700

    Fix: BUG

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 sudo service ssh start
 sudo service cron start
 
-echo "CTF Lab ready. SSH on port 2222."
+echo -e "CTF Lab ready. SSH on port 3333 how to login?\nssh analyst@localhost -p 3333"
 tail -f /dev/null
